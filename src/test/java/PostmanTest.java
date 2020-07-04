511b0561a696691fcabc7db717fe2fb51ac3a971
asrodionov <arodionov27@yandex.ru>
2020-07-04 20:54:25 +0300

    Initial commit

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanTest {
+    @Test
+    void shouldPostData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                // Выполняемые действия
+                .when()
+                    .post("/post")
+                // Проверки
+                .then()
+                    //.statusCode(200)
+                    .assertThat().body("data", equalTo("some dat"))
+        ;
+    }
+}
